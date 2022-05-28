c35b6ffd3a12ad6897084a157704451677e1db02
FilipSokol <sok.filip@gmail.com>
2022-05-28 21:27:37 +0200

    first commit

---
+++ LabPDFVal/Program.cs
@@ -0,0 +1,47 @@
+﻿using Spire.Pdf;
+using Spire.Pdf.Security;
+using Spire.Pdf.Widget;
+using System.Security.Cryptography.X509Certificates;
+
+string filename = @"C:\Users\sokfi\OneDrive\Dzieło sztuki\SQL Zajecia\Lab 4\LabPDFVal\LabPDFVal\LabPDFVal\podpis1.pdf";
+//string filename = @"C:\Users\sokfi\OneDrive\Dzieło sztuki\SQL Zajecia\Lab 4\LabPDFVal\LabPDFVal\LabPDFVal\podpis2.pdf";
+//string filename = @"C:\Users\sokfi\OneDrive\Dzieło sztuki\SQL Zajecia\Lab 4\LabPDFVal\LabPDFVal\LabPDFVal\brakpodp.pdf";
+
+List<PdfSignature> signatures = new List<PdfSignature>();
+PdfDocument doc = new PdfDocument(filename);
+PdfFormWidget form = (PdfFormWidget)doc.Form;
+
+
+    if (form == null)
+    {
+        Console.WriteLine("PLIK NIE POSIADA PODPISU!");
+    }
+    else
+    {
+
+    for (int i = 0; i < form.FieldsWidget.Count; ++i)
+
+    {
+        PdfSignatureFieldWidget? field = form.FieldsWidget[i] as PdfSignatureFieldWidget;
+
+        if (field != null && field.Signature != null)
+
+        {
+            PdfSignature signature = field.Signature;
+            signatures.Add(signature);
+            PdfSignature signatureOne = signatures[0];
+            X509Certificate2 certificate = signatureOne.Certificate as X509Certificate2;
+
+            string subject = certificate.Subject;
+            DateTime validStart = certificate.NotBefore;
+            DateTime validEnd = certificate.NotAfter;
+
+            Console.WriteLine("PLIK POSIADA PODPIS!\n============================================");
+            Console.WriteLine("Info - " + subject);
+            Console.WriteLine("Data podpisu - " + validStart);
+            Console.WriteLine("Data wygasniecia podpisu - " + validEnd + "\n============================================");
+        }
+    }
+}
+
+// dotnet add package FreeSpire.PDF --version 8.2.0
